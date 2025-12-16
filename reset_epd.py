b03b340d5e46ce6bfe8856ed5520719bc5c55e05
rasberryrabbit <purplecofe2@gmail.com>
2025-12-16 22:20:28 +0900

    Reset epaper on ESP32.

---
+++ reset_epd.py
@@ -0,0 +1,18 @@
+
+from machine import Timer, Pin, I2C, RTC, SPI
+import micropython, re, time, network, socket, ntptime, configreader, random, ssl, uos
+import errno, sys, ubinascii, machine
+
+
+import epd1n54v2
+import framebuf
+spi=SPI(2, sck=Pin(18), mosi=Pin(23))
+spi.init()
+e = epd1n54v2.EPD(spi, cs=Pin(14), dc=Pin(12), rst=Pin(13), busy=Pin(4))
+e.fast=False
+e.init()
+
+dispbuf=bytearray(e.width // 8 * e.height)
+disp=framebuf.FrameBuffer(dispbuf, e.width, e.height, framebuf.MONO_HLSB)
+
+e.clear()
