6e09fa72f821c7321999a3e00e5797b0076b4ac0
rasberryrabbit <purplecofe2@gmail.com>
2025-12-16 22:29:40 +0900

    Update epaper example.

--- reset_epd.py
+++ reset_epd.py
@@ -9,10 +9,15 @@ import framebuf
 spi=SPI(2, sck=Pin(18), mosi=Pin(23))
 spi.init()
 e = epd1n54v2.EPD(spi, cs=Pin(14), dc=Pin(12), rst=Pin(13), busy=Pin(4))
-e.fast=False
 e.init()
 
 dispbuf=bytearray(e.width // 8 * e.height)
 disp=framebuf.FrameBuffer(dispbuf, e.width, e.height, framebuf.MONO_HLSB)
 
 e.clear()
+
+#disp.fill(1)
+#disp.text('hello world!',30,0,0)
+#for i in range(200 // 8):
+#    disp.text('%d' % i,0,i*8,0)   
+#e.display(dispbuf)
