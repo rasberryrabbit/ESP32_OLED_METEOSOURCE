03399759081a35bd45306a8c394f6f9de231ef29
rasberryrabbit <purplecofe2@gmail.com>
2025-12-17 18:44:59 +0900

    Update test example.

--- reset_epd.py
+++ reset_epd.py
@@ -10,11 +10,15 @@ import framebuf
 spi=SPI(2, sck=Pin(18), mosi=Pin(23))
 spi.init()
 e = epd1n54v2.EPD(spi, cs=Pin(14), dc=Pin(12), rst=Pin(13), busy=Pin(4))
-e.partial=False
 e.init()
 
 dispbuf=bytearray(e.width // 8 * e.height)
 disp=framebuf.FrameBuffer(dispbuf, e.width, e.height, framebuf.MONO_HLSB)
+
+disp.fill(1)
+e.display_part_base_image(dispbuf)
+e.display_part_base_image(dispbuf)
+
 
 def drawtxt(x,y):
     data=bytearray(8)
@@ -29,19 +33,9 @@ x=1
 def doit():
     # draw image
     global x
-    drawtxt(100+x,100)
+    drawtxt(100+x*8,100)
     e.display_part_frame()
     x=x+1
-
-# draw base image on framebuf
-disp.fill(1)
-disp.text('hello world!',30,0,0)
-for i in range(200 // 8):
-    disp.text('%d' % i,0,i*8,0)
-
-# partial has 2 memory page, fill the pages
-e.display_part_base_white_image()
-e.display_part_base_white_image()
 
 # partial mode
 e.init(True)
